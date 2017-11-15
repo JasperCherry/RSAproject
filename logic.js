f870dfe19b2dbe51d442d65214abc0359c65dcf8
unknown <kacper.wisnicki@gmail.com>
2017-11-15 07:26:05 +0000

    basic use with wolfram aplha api

--- logic.js
+++ logic.js
@@ -1,32 +1,104 @@
+// alice data
 let p;
 let q;
 let n;
 let ϕn;
 let e;
 let d = 0;
+let m;
+let msgSent;
+
+// control
+let genKeys = false;
+let keyInt = false;
+let msgInt = true;
+
+// bob data
+let bobN;
+let bobE;
+let bobMsg;
 
 function usePrimes() {
   p = document.getElementById("p").value;
   q = document.getElementById("q").value;
   if (p == '' || q == '') {
-    alert("You need to enter primes first");
+    alert("You need to enter both primes");
   } else {
-    n = p * q;
-    document.getElementById("n").innerHTML = "n=" + n;
-    ϕn = (p - 1) * (q - 1);
-    document.getElementById("ϕn").innerHTML = "ϕ(n)=" + ϕn;
+    if (!checkIfPrime(p)) {
+      alert("p is not a prime");
+    } else if (!checkIfPrime(q)) {
+      alert("q is not a prime");
+    } else {
+      n = p * q;
+      document.getElementById("n").innerHTML = "n=" + n;
+      ϕn = (p - 1) * (q - 1);
+      document.getElementById("ϕn").innerHTML = "ϕ(n)=" + ϕn;
+    }
+
   }
 }
 
 function generateD() {
   e = document.getElementById("e").value;
-  while (((e * d) % ϕn) != 1) {
-    d++;
+  if (n == null || ϕn == null) {
+    alert("You need to generate n and ϕn first");
+  } else if (e == '') {
+    alert("Enter your e first");
+  } else if (e <= 0 || e >= ϕn) {
+    alert("e has to be larger then 0 and smaller then ϕn");
+  } else {
+    if (!(checkIfPrime(e))) {
+      alert("e has to be prime");
+    } else {
+      while (((e * d) % ϕn) != 1) {
+        d++;
+      }
+      document.getElementById("d").innerHTML = "d=" + d;
+    }
   }
-  document.getElementById("d").innerHTML = "d=" + d;
 }
 
 function generateKeys() {
-  document.getElementById("private").innerHTML = 'private key (d,n) -> (' + d + ',' + n + ')';
-  document.getElementById("public").innerHTML = 'public key (e,n) -> (' + e + ',' + n + ')';
+  if (d == 0) {
+    alert("You need to generate d first");
+  } else {
+    genKeys = true;
+    document.getElementById("private").innerHTML = 'private key (d,n) -> (' + d + ',' + n + ')';
+    document.getElementById("public").innerHTML = 'public key (e,n) -> (' + e + ',' + n + ')';
+  }
+}
+
+function checkIfPrime(number) {
+  if (number < 2) {
+    return false;
+  }
+  for (let x = 2; x < (number / 2) + 1; x++) {
+    if (number % x == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function sendPublicKey() {
+  if (!genKeys) {
+    alert("You need to generate key first");
+  } else {
+    bobN = n;
+    bobE = e;
+    document.getElementById("bobKey").innerHTML = '(e,n) -> (' + e + ',' + n + ')';
+  }
+}
+
+function sendMessage() {
+  msg = document.getElementById("msg").value;
+  if (bobN == null || bobE == null) {
+    alert("You need to send Bob the public key first");
+  } else if (msg == '') {
+    alert("The message is empty");
+  } else {
+    // needs wolfram alpha
+
+
+  }
 }
--- server.js
+++ server.js
@@ -1,26 +1,11 @@
-var express = require('express');
-var app = express();
-// using router to separate url
-var router = express.Router();
-// data exchange configuration
-app.use(function(req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
-});
-// start express server
-var server = app.listen(8080, function() {
-    console.log('Server is running at : ' + server.address().port);
-});
-app.get('/firstCall', function(req, res) {
-    console.log("confirmation - first call has been made");
-    res.send({'msg':'first call has been made'});
+var Client = require('node-wolfram');
+var Wolfram = new Client('LU2W26-EPRQKKG36V');
+Wolfram.query("(3^37) mod 119", function(err, result) {
+    if(err)
+        console.log(err);
+    else
+    {
+      console.log(result.queryresult.pod[1].subpod[0].plaintext[0]);
+      console.log("<><><><><><><><><><><><><><><><><><><><>");
+    }
 });
